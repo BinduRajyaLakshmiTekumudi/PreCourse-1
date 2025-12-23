8dd1460dd2dfc1d05245c2f0a5f1d7878dbb53bb
B1R2L3 <bindurajyalakshmi@gmail.com>
2025-12-22 16:22:22 -0800

    Added comments to Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,12 +1,28 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
+  #Time Complexity : O(1)
+  #Space Complexity :O(1)
+  #Did this code successfully run on Leetcode :
+  #Any problem you faced while coding this : Have learned concept of stack
+
+  #Your code here along with comments explaining your approachclass myStack:
+  #Stack -LIFO
+  #isEmpty- checking if the length of the stack is 0, return True else False
+  # Push - Adding element to the top
+  # Pop - Removing the element from the top
+  # Peek - Accessing the top element
+  # size - return the length of the stack
+  # show - displays the stack
+
      def __init__(self):
           self.stack=[]
-         
+
      def isEmpty(self):
-          return(len(self.stack))==0
-         
+         if len(self.stack) == 0:
+             return True
+         return False
+
      def push(self, item):
          self.stack.append(item)
 
@@ -14,12 +30,12 @@ class myStack:
         if self.isEmpty():
             return None
         return self.stack.pop()
-        
+
      def peek(self):
         if self.isEmpty():
              return None
         return self.stack[-1]
-        
+
      def size(self):
          return  len(self.stack)
      def show(self):
